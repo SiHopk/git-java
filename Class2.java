493e11bd75486b928a2665442cbc910ef6b06b1d
simon <shopkins2@stu.jsu.edu>
2020-08-23 16:38:14 -0400

    Initial Commit

---
+++ Class2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworl;
+
+/**
+ *
+ * @author simon
+ */
+public class Class2 {
+    private void f1() {
+        // Placeholder method (v1.0)    
+    }
+}
---
+++ Helloworl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworl;
+
+/**
+ *
+ * @author simon
+ */
+public class Helloworl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
+        System.out.println("Hello, World!");
+    }
+    private void f1() {
+        // Placeholder method (v1.0)}
+    }
+    
+}
